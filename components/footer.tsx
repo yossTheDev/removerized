828211e207b3864bced51ea6beeff7e7306627a8
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-19 07:58:51 -0400

    style: :lipstick: Add More Information in the Footer Section

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,29 @@
 export const Footer = () => {
   return (
-    <span className="p-2 text-center text-sm text-neutral-400">
-      Made by{" "}
-      <a className="hover:underline" href="https://yossthedev.github.io">
+    <span className="p-2 text-sm text-neutral-600">
+      Built by{" "}
+      <a
+        className="hover:underline"
+        target="_blank"
+        href="https://yossthedev.github.io"
+      >
         @yossthedev
+      </a>
+      . Icons from{" "}
+      <a
+        className="hover:underline"
+        target="_blank"
+        href="https://www.figma.com/community/file/1166831539721848736"
+      >
+        Solar Icons
+      </a>
+      {" - "}
+      <a
+        className="hover:underline"
+        target="_blank"
+        href="https://creativecommons.org/licenses/by/4.0/"
+      >
+        CC BY 4.0 Licence
       </a>
     </span>
   )
