1d9570abcc9fc1af09190392651342a28baab21a
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-19 08:32:51 -0400

    style: :lipstick: Add tada Effect on Hover Header Text

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default async function IndexPage() {
   return (
     <section className="container flex h-full  flex-col gap-2 pb-8 md:flex-row">
       {/* Hero */}
-      <div className="flex flex-col items-center justify-center md:w-2/4">
+      <div className="group flex flex-col items-center justify-center md:w-2/4">
         <div className="flex md:w-96">
           <h1 className="mt-14 animate-fade-in-up text-center text-4xl font-bold md:text-start md:text-6xl lg:text-7xl">
             <span>Free Background Remover Tool with</span>
             <br></br>
             <span>AI</span>
             <span className="inline-block">
-              <Icons.SolarStarsBoldDuotone className="flex"></Icons.SolarStarsBoldDuotone>
+              <Icons.SolarStarsBoldDuotone className="flex group-hover:animate-tada"></Icons.SolarStarsBoldDuotone>
             </span>
           </h1>
         </div>
