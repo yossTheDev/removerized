7c93e401fbfe31f93b7b4ad86d74f8a85e41daf0
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-23 20:31:16 -0400

    feat: :rocket: Enable cross-origin isolation

--- next.config.mjs
+++ next.config.mjs
@@ -23,4 +23,21 @@ export default withSerwist({
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 });
