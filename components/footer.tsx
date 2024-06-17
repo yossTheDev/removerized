f6a9b02c3c619695d03ec37a87bb64ace37a7fd2
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-06-17 11:22:01 -0400

    style: :lipstick: Update Footer Style

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,21 @@
 export const Footer = () => {
   return (
-    <span className="mx-20 p-4 text-center text-xs text-neutral-600 md:mx-0 md:p-2 md:text-start">
+    <span className="border-t p-4 text-center text-xs text-muted-foreground md:mx-0 md:p-6 md:text-start">
       Built by{" "}
       <a
-        className="hover:underline"
+        className="text-foreground hover:underline"
         target="_blank"
         href="https://yossthedev.github.io"
       >
         @yossthedev
       </a>
-      . Icons from{" "}
+      . The source code is available on{" "}
       <a
-        className="hover:underline"
+        className="text-foreground hover:underline"
         target="_blank"
-        href="https://www.figma.com/community/file/1166831539721848736"
+        href="https://github.com/yossTheDev/removerized"
       >
-        Solar Icons
-      </a>
-      {" - "}
-      <a
-        className="hover:underline"
-        target="_blank"
-        href="https://creativecommons.org/licenses/by/4.0/"
-      >
-        CC BY 4.0 Licence
+        GitHub
       </a>
     </span>
   )
