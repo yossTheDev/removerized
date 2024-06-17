e8266885477f90bd89199c87c7bca27ef6dd6f65
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-06-17 12:00:49 -0400

    update: Site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Removerized - Free AI-Powered Background Remover Tool",
+  name: "removerized: Free AI-Powered Background Remover Tool",
 
   description:
     "Effortlessly Remove Image Backgrounds with AI - Free & Limitless with Offline Support",
