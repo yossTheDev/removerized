8fe8dfda7e5d3098653f45d7590bb013fcc5b4be
Yoannis Sánchez Soto <yoannisgnw@gmail.com>
2024-11-16 06:37:50 -0500

    fix: :bug: Minor Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,6 @@ import { siteConfig } from "@/config/site"
 import { fontMuseo, fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/sonner"
-import { Footer } from "@/components/footer"
-import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
