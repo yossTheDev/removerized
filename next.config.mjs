21f95a89726cc8e8b7953ac6a196a83a58b1122a
Yoannis Sanchez Soto <yossthedev@gmail.com>
2026-03-18 16:45:34 -0400

    Remove unnecessary Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
 /** @type {import('next').Config} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    esmExternals: "loose",
-  },
   serverRuntimeConfig: {
     runtime: "edge",
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "onnxruntime-node": false,
-    }
-
-    config.module.noParse = /onnxruntime-web/
-
-    return config
   },
   images: {
     dangerouslyAllowSVG: true,
